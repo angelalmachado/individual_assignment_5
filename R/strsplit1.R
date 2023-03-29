247f551234fb47de67c8f3241be1cb620df5f12a
Angela Machado <angelalopesmachado@gmail.com>
2023-03-28 21:35:53 -0700

    Updated roxygen description, DESCRIPTION file and NAMESPACE

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -3,11 +3,11 @@
 #' @param x A character vector containing the strings to split.
 #' @param split A character vector containing the delimiter to use for splitting the strings.
 #'
-#' @return A list containing the substrings resulting from the split operation.
+#' @return A character vector.
+#' @export
 #'
 #' @examples
-#' strsplit("hello,world", ",")
-#' strsplit("hello,world", ",", fixed = TRUE)
+#' strsplit1("hello,world", ",")
 
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
